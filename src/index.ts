9d140ab9322a5d2d05c3335701d0fd735ec93e46
Alias Qli <2576814881@qq.com>
2024-04-12 21:50:52 +0800

    use CIE94

--- src/index.ts
+++ src/index.ts
@@ -29,44 +29,48 @@ const localStorage = new LocalStorage('./extra-storage.db');
 const BOT_UPDATE_VERSION = 1;
 
 const colors = {
-    "🔴": [221, 46, 68], // [R, G, B]
-    "🔵": [85, 172, 238],
-    "🟠": [244, 144, 12],
-    "🟡": [253, 203, 88],
-    "🟢": [120, 177, 89],
-    "🟣": [170, 142, 214],
-    "🟤": [193, 105, 79],
-    "⚫": [49, 55, 61],
-    "⚪": [230, 231, 232],
+    "🔴": rgb2lab([221, 46, 68]),
+    "🔵": rgb2lab([85, 172, 238]),
+    "🟠": rgb2lab([244, 144, 12]),
+    "🟡": rgb2lab([253, 203, 88]),
+    "🟢": rgb2lab([120, 177, 89]),
+    "🟣": rgb2lab([170, 142, 214]),
+    "🟤": rgb2lab([193, 105, 79]),
+    "⚫": rgb2lab([49, 55, 61]),
+    "⚪": rgb2lab([230, 231, 232]),
 };
 
-// h in radians, c and v in [0, 255], c = v * s
-function rgbToHcv(rgb: number[]): number[] {
-    const [r, g, b] = rgb;
-    const min = Math.min(r, g, b);
-    const max = Math.max(r, g, b);
-    const c = max - min;
-    let h;
-    if (c == 0) {
-        h = 0;
-    } else if (r >= g && r >= b) {
-        h = (g - b) / c % 6;
-    } else if (g >= r && g >= b) {
-        h = (b - r) / c + 2;
-    } else {
-        h = (r - g) / c + 4;
-    }
-    h *= Math.PI / 3;
-    const v = max;
-    // const s = v == 0 ? 0 : 255 * c / v;
-    return [h, c, v];
+//converse rgb to lab
+function rgb2lab(rgb: number[]): number[]{
+    let r = rgb[0] / 255, g = rgb[1] / 255, b = rgb[2] / 255, x, y, z;
+    r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+    g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+    b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+    x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+    y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+    z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+    x = (x > 0.008856) ? Math.pow(x, 1/3) : (7.787 * x) + 16/116;
+    y = (y > 0.008856) ? Math.pow(y, 1/3) : (7.787 * y) + 16/116;
+    z = (z > 0.008856) ? Math.pow(z, 1/3) : (7.787 * z) + 16/116;
+    return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)]
 }
 
-function hcvDistance(a: number[], b: number[]): number {
-    const [h1, c1, v1] = a;
-    const [h2, c2, v2] = b;
-    const deltav = v1 - v2;
-    return c1 * c1 + c2 * c2 + deltav * deltav - 2 * c1 * c2 * Math.cos(h1 - h2);
+function cie94(labA: number[], labB: number[]) {
+    var deltaL = labA[0] - labB[0];
+    var deltaA = labA[1] - labB[1];
+    var deltaB = labA[2] - labB[2];
+    var c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+    var c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+    var deltaC = c1 - c2;
+    var deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+    deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+    var sc = 1.0 + 0.045 * c1;
+    var sh = 1.0 + 0.015 * c1;
+    var deltaLKlsl = deltaL / (1.0);
+    var deltaCkcsc = deltaC / (sc);
+    var deltaHkhsh = deltaH / (sh);
+    var i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+    return i < 0 ? 0 : Math.sqrt(i);
 }
 
 let agent: SocksProxyAgent | any | undefined = undefined;
@@ -327,23 +331,24 @@ new Cli({
                         const req = await fetch(url);
                         const buffer = await req.arrayBuffer();
                         const img = await Jimp.read(Buffer.from(buffer));
-                        let sum = [0, 0, 0];
-                        let counter = 0;
+                        let colorFreq: Record<string, number> = {};
                         for (let x = 0; x < img.getWidth(); x++) {
                             for (let y = 0; y < img.getHeight(); y++) {
                                 const {r, g, b, a} = Jimp.intToRGBA(img.getPixelColor(x, y));
-                                if (!(a == 0 || r == 255 && g == 255 && b == 255)) {
-                                    sum[0] += r * (a/255);
-                                    sum[1] += g * (a/255);
-                                    sum[2] += b * (a/255);
-                                    counter++;
-                                }
+                                const color256 = [r, g, b].map(x => x * (a/255) + 255 * (1-a/255));
+                                const color16 = color256.map(x => Math.round(x / 255 * 15));
+                                colorFreq[JSON.stringify(color16)] = (colorFreq[JSON.stringify(color16)] ?? 0) + 1;
                             }
                         }
-                        const hcv = rgbToHcv(counter == 0 ? [255, 255, 255] : sum.map(v => v / counter));
+                        const rgb16 = Object.entries(colorFreq).map(kv => {
+                            const [k, freq] = kv;
+                            const [r, g, b]: number[] = JSON.parse(k);
+                            return {color16: [r, g, b], weight: (/* chroma */ Math.max(r, g, b) - Math.min(r, g, b)) * Math.log2(freq)};
+                        }).sort((a, b) => a.weight - b.weight).reverse()[0].color16;
+                        const lab = rgb2lab(rgb16.map(x => x / 15 * 255));
                         return Object.entries(colors).sort(
-                            (a, b) => hcvDistance(rgbToHcv(a[1] as number[]), hcv) 
-                                    - hcvDistance(rgbToHcv(b[1] as number[]), hcv)
+                            (a, b) => cie94(a[1] as number[], lab)
+                                    - cie94(b[1] as number[], lab)
                             )[0][0] as string;
                     }
                     if (!prev_name_dict[event.room_id])
