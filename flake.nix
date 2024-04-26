e6256b25aafe4af5102b47c9cf15feb45e19da61
gjz010 <gjz010944@gmail.com>
2024-04-26 11:03:04 +0000

    wip

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  description = "A very basic flake";
 
   outputs = { self, nixpkgs }: {
     devShells.x86_64-linux.default = let pkgs = import nixpkgs {system = "x86_64-linux"; }; in pkgs.mkShell{
-        buildInputs = [pkgs.nodejs pkgs.bashInteractive ];
+        buildInputs = [pkgs.nodejs_21 pkgs.bashInteractive ];
+        nativeBuildInputs = [pkgs.biome];
     };
 
   };
