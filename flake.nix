34a0e61ec38d2be5bba5d28af59395a594167399
spore <spore2050@gmail.com>
2024-11-11 18:03:34 +0800

    Add wasm toolchain to flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,31 @@
 {
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = let pkgs = import nixpkgs {system = "x86_64-linux"; }; in pkgs.mkShell{
-        buildInputs = [pkgs.nodejs pkgs.bashInteractive pkgs.proxychains-ng pkgs.ffmpeg-full pkgs.imagemagick ];
+  outputs = { self, nixpkgs, rust-overlay }: {
+    devShells.x86_64-linux.default = let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs { system = "x86_64-linux"; inherit overlays; };
+    in pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs
+        biome
+        ffmpeg-full
+        imagemagick
+        bashInteractive
+        (rust-bin.stable.latest.default.override {
+          extensions = ["rust-src" "rust-analyzer"];
+          targets = ["wasm32-unknown-unknown"];
+        })
+        wasm-bindgen-cli
+        wasm-pack
+      ];
     };
-
   };
 }
